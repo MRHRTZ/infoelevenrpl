08192487c7e7afc7b2be76448c95537104bd2eb3
MRHRTZ <52845610+MRHRTZ@users.noreply.github.com>
2020-08-09 05:31:37 +0700

    Add files via upload

---
+++ INSTA/igdl.php
@@ -0,0 +1,61 @@
+<?php  
+	require"api.php";
+
+	$req = file_get_contents('php://input');
+	$data = json_decode($req);
+	$masuk = $data->query->message;
+	$param = explode(" ", $masuk, 2);
+//	print_r($param);
+	$url = ucfirst($param[1]);
+
+
+$ua = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36";
+ini_set("user_agent", $ua);
+
+//$url = isset($_GET['url']) ? $_GET['url'] : "";
+
+if (!empty($url)) {
+    if (checkUrl($url)) {
+        header('Content-Type:application/json');
+        echo genLink(file_get_contents($url));
+        exit;
+    } else {
+        header('Content-Type:application/json');
+        // $out = array(
+        //         "status"  => "error",
+        //         "err_msg" => "invalid instagram url!",
+        //        );
+	   $outgoing = '
+	{
+	"replies": [
+	    {
+	        "message": "*Kesalahan! Tautan tidak valid, gunakan tautan instagram yang benar*"
+	    }
+	]
+	}
+	';
+
+	echo $outgoing;
+    }
+} else {
+    header('Content-Type:application/json');
+    // $out = array(
+    //         "status"  => "error",
+    //         "err_msg" => "no input!",
+    //        );
+
+            $outgoing = '
+        {
+            "replies": [
+                {
+                    "message": "*Tidak ada tautan yang terdeteksi!*\\nMasukan link instagram setelah /igdl."
+                }
+            ]
+        }
+        ';
+
+        echo $outgoing;
+}//end if
+
+
+?>
